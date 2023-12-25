2172699c4f85cffe720d913ec626f92ade81bfb7
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-12-25 20:36:00 +0300

    Add project files.

---
+++ CQRS_Project/DAL/Context/SaleContext.cs
@@ -0,0 +1,17 @@
+﻿using CQRS_Project.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Principal;
+
+namespace CQRS_Project.DAL.Context
+{
+    public class SaleContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = DESKTOP-JNJNVEQ\\MERTSQL;initial catalog=SaleDb;integrated security=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
